fd18d04170ed6aa3531d2d3760580b72c050092a
yashChouhanGit <yashchouhan@371@gmail.com>
2024-02-17 18:40:11 +0530

    Only for testing

--- Sources/BlackBGViewPackage/BlackBGViewPackage.swift
+++ Sources/BlackBGViewPackage/BlackBGViewPackage.swift
@@ -1,5 +1,5 @@
 public struct BlackBGViewPackage {
-    public private(set) var text = "Hello, Yash , I am first package , it has been created by you"
+    public private(set) var text = "Hello, Yash , I am first package , it has been created by you a new changes"
 
     public init() {
         
